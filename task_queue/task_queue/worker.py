fab1049b83131ba6cb15ffa0408f40053a2eb011
stellahsr <stellahsr@126.com>
2024-03-26 15:32:49 +0800

    update worker.py

--- task_queue/task_queue/worker.py
+++ task_queue/task_queue/worker.py
@@ -5,8 +5,11 @@
 @Author  : alexanderwu
 @File    : worker.py
 """
-
+import os
+import subprocess
 from celery import Celery
+
+from metrics.report import get_model_report
 
 # 配置 Celery 使用 Redis 作为消息代理
 app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
@@ -17,4 +20,9 @@ def execute_task(x):
     # 模拟一个耗时的计算任务
     import time
     time.sleep(5)
-    return (x, x * x)
+    os.chdir("/evaluation/harness")
+    os.system(". run_eval.sh")
+    # result = get_model_report(model="", predictions_path="", swe_bench_tasks="", log_dir="")
+    # todo update result to redis
+    # return result
+    return x
